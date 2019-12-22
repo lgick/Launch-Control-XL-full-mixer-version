fa66808f4edbf7f52c148af5b1e8661327dbe242
lgick <lgick@yandex.ru>
2019-12-22 17:55:06 +0300

    device buttons

--- DeviceComponent.py
+++ DeviceComponent.py
@@ -6,22 +6,79 @@
 # Compiled at: 2019-04-09 19:23:44
 from __future__ import absolute_import, print_function, unicode_literals
 import Live
+from itertools import izip_longest
 from _Framework.Control import control_list, ButtonControl
 from _Framework.DeviceComponent import DeviceComponent as DeviceComponentBase
+from _Framework.SubjectSlot import subject_slot, subject_slot_group, Subject
+
+import functools, logging, traceback
+logger = logging.getLogger(__name__)
+#logger.error('#### !!!!!!!!!!! #########')
 
 class DeviceComponent(DeviceComponentBase):
     parameter_lights = control_list(ButtonControl, control_count=8, enabled=False, color='Color.DeviceControlOn', disabled_color='Color.DeviceControlOff')
     prev_device_button = ButtonControl()
     next_device_button = ButtonControl()
     reset_device_button = ButtonControl()
+    device_buttons = control_list(ButtonControl, control_count=8)
 
     def clear_buttons(self):
+        for button in self.device_buttons:
+            if button != None:
+                button.color = 'Color.Off'
+                button.set_control_element(None)
+
         self.prev_device_button.color = 'Color.Off'
         self.next_device_button.color = 'Color.Off'
         self.reset_device_button.color = 'Color.Off'
         self.prev_device_button.set_control_element(None)
         self.next_device_button.set_control_element(None)
         self.reset_device_button.set_control_element(None)
+
+    @reset_device_button.pressed
+    def reset_device_button(self, button):
+        pass
+
+    @prev_device_button.pressed
+    def prev_device_button(self, button):
+        self._scroll_device_view(Live.Application.Application.View.NavDirection.left)
+
+    @next_device_button.pressed
+    def next_device_button(self, button):
+        self._scroll_device_view(Live.Application.Application.View.NavDirection.right)
+
+    @device_buttons.pressed
+    def device_buttons(self, button):
+        index = button.index
+        track = self.song().view.selected_track
+
+        if len(track.devices) > index:
+            device_to_select = track.devices[index]
+            self.song().view.select_device(device_to_select)
+            self.set_device(device_to_select)
+
+    @subject_slot('selected_device')
+    def __on_selected_device_changed(self):
+        super(DeviceComponent, self).__on_selected_device_changed()
+        self.update_device_buttons()
+
+    def update_device_selection(self):
+        super(DeviceComponent, self).update_device_selection()
+        self.update_device_buttons()
+
+    def update_device_buttons(self):
+        track = self.song().view.selected_track
+        device_to_select = track.view.selected_device
+
+        for button, device in izip_longest(self.device_buttons, track.devices):
+            if button:
+                if device == None:
+                    button.color = 'Color.Off'
+                else:
+                    if device == device_to_select:
+                        button.color = 'Color.DeviceOn'
+                    else:
+                        button.color = 'Color.DeviceOff'
 
     def set_prev_device_button(self, button):
         if button:
@@ -38,23 +95,18 @@ class DeviceComponent(DeviceComponentBase):
             self.reset_device_button.set_control_element(button)
             self.reset_device_button.color = 'Color.On'
 
-    @reset_device_button.pressed
-    def reset_device_button(self, button):
-        pass
-
-    @prev_device_button.pressed
-    def prev_device_button(self, button):
-        self._scroll_device_view(Live.Application.Application.View.NavDirection.left)
-
-    @next_device_button.pressed
-    def next_device_button(self, button):
-        self._scroll_device_view(Live.Application.Application.View.NavDirection.right)
-
     def set_on_off_button(self, button):
         if button:
             button.set_on_off_values('Color.DeviceOn', 'Color.DeviceOff')
 
         super(DeviceComponent, self).set_on_off_button(button)
+
+    def set_device_buttons(self, buttons):
+        if buttons:
+            for device_button, button in izip_longest(self.device_buttons, buttons):
+                device_button.set_control_element(button)
+
+            self.update_device_buttons()
 
     def _scroll_device_view(self, direction):
         self.application().view.show_view('Detail')
@@ -69,7 +121,7 @@ class DeviceComponent(DeviceComponentBase):
     def set_bank_buttons(self, buttons):
         for button in buttons or []:
             if button:
-                button.set_on_off_values('Color.NavDown', 'Color.Off')
+                button.set_on_off_values('Color.On', 'Color.Off')
 
         super(DeviceComponent, self).set_bank_buttons(buttons)
 
